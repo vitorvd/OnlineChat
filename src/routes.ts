d1b33c22ea9507be216b863d980064f483fe1f5a
vitorvd <vitordversiani@gmail.com>
2021-04-21 11:47:57 -0300

    updating project to sync with class 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsConstroller } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 
 const routes = Router();
 
-const settingController = new SettingsConstroller();
+const settingsController = new SettingsConstroller();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
-routes.post('/settings', settingController.create);
+routes.post('/settings', settingsController.create);
+
+routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes }
